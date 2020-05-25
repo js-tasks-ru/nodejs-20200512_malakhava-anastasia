760fc1d976c37e8e5dea5c89444c091befa53dce
Anastasia Malakhova <amalakhova.xbs@gmail.com>
2020-05-25 22:32:04 +0300

    add solution for 4-module 1-task

--- 4-module/1-task/server.js
+++ 4-module/1-task/server.js
@@ -1,6 +1,7 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
+const fs = require('fs');
 
 const server = new http.Server();
 
@@ -8,10 +9,25 @@ server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
   const filepath = path.join(__dirname, 'files', pathname);
+  const readableStream = fs.createReadStream(filepath);
+
+  readableStream.on('open', () => {
+    readableStream.pipe(res);
+  });
+
+  readableStream.on('error', (err) => {
+    if (err.code === 'ENOENT') {
+      res.statusCode = 404;
+    }
+    if (pathname.includes('/')) {
+      res.statusCode = 400;
+    }
+    readableStream.destroy();
+    res.end();
+  });
 
   switch (req.method) {
     case 'GET':
-
       break;
 
     default:
